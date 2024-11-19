d126dbdcc7eb2306ffac07fb7958eb642b6f3408
allDolinin <allDolinin@gmail.com>
2024-11-19 11:56:56 +0300

    add SwiftUI application lifecycle

--- bb_sui/bb_sui/bb_suiApp.swift
+++ bb_sui/bb_sui/bb_suiApp.swift
@@ -2,9 +2,25 @@ import SwiftUI
 
 @main
 struct bb_suiApp: App {
+    
+    @Environment(\.scenePhase) private var scenePhase
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+            
+                .onChange(of: scenePhase) { oldPhase, newPhase in
+                    switch newPhase {
+                    case .background:
+                        print("SchenePhase: Background from \(oldPhase)")
+                    case .inactive:
+                        print("SchenePhase: Inactive from \(oldPhase)")
+                    case .active:
+                        print("SchenePhase: Active/Foreground from \(oldPhase)")
+                    @unknown default:
+                        print("SchenePhase: Unknown scene phase \(newPhase) from \(oldPhase)")
+                    }
+                }
         }
     }
 }
